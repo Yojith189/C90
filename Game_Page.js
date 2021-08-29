d7ef0961b20d250d7ecd2ea95004a47d53438f6a
Yojith189 <77870739+Yojith189@users.noreply.github.com>
2021-08-29 09:58:18 +0530

    Add files via upload

---
+++ Game_Page.js
@@ -0,0 +1,5 @@
+playerone_name = localStorage.getItem("playerone_name");
+playertwo_name = localStorage.getItem("playertwo_name");
+
+document.getElementById("playerone_name").innerHTML=playerone_name;
+document.getElementById("playertwo_name").innerHTML=playertwo_name;
